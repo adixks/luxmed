483ed52502b78f10ab40ac0bf50a33ca95af25d6
adixks <adrianks@op.pl>
2024-10-07 00:37:42 +0200

    Add services and repositories

---
+++ src/main/java/com/example/luxmed/controller/CompanyController.java
@@ -0,0 +1,46 @@
+package com.example.luxmed.controller;
+
+import com.example.luxmed.dto.CompanyDto;
+import com.example.luxmed.service.CompanyService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/companies")
+@Validated
+public class CompanyController {
+    private CompanyService companyService;
+
+    @GetMapping
+    public List<CompanyDto> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+
+    @GetMapping("/{id}")
+    public CompanyDto getCompanyById(@PathVariable Long id) {
+        return companyService.getCompanyById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<CompanyDto> createCompany(@Valid @RequestBody CompanyDto companyDto) {
+        return new ResponseEntity<>(companyService.createCompany(companyDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public CompanyDto updateCompany(@PathVariable Long id, @Valid @RequestBody CompanyDto companyDto) {
+        return companyService.updateCompany(id, companyDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.noContent().build();
+    }
+}
