5a7f4afcc40aea16060908547f7df39e0df9d67f
adixks <adrianks@op.pl>
2024-10-06 22:10:01 +0200

    Add basic element of application

---
+++ src/main/java/com/example/luxmed/LuxmedApplication.java
@@ -0,0 +1,13 @@
+package com.example.luxmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxmedApplication.class, args);
+	}
+
+}
